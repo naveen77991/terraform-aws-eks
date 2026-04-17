74c47badc993edc0ccdaa98810fb245ca5e90c4e
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 15:54:57 +0530

    Update data.tf

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,28 +1,10 @@
 data "aws_ami" "joindevops" {
-  owners           = ["973714476881"]
-  most_recent      = true
+  most_recent = true
+
+  owners = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-
-data "aws_ssm_parameter" "bastion_sg_id" {
-  name = "/${var.project}/${var.environment}/bastion_sg_id"
-}
-
-data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project}/${var.environment}/public_subnet_ids"
-}
-
