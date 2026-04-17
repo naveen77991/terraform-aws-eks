de74e183ce823b9e357114c2b8c57700da9fbd30
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 11:06:25 +0530

    Update provider.tf

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "latest-84s-remote-state-dev-6792"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
     encrypt        = true
