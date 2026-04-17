57456a34a45966e92dc41680db237b3f247fce7c
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 15:44:05 +0530

    Update provider.tf

--- 60-acm/provider.tf
+++ 60-acm/provider.tf
@@ -2,16 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.98.0"
     }
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "s3bucketpolicytest-6792"
     key    = "roboshop-dev-acm"
     region = "us-east-1"
-    encrypt        = true
-    use_lockfile = true
+    
   }
 }
 
