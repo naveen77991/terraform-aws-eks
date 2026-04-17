93ce83ebaf24cc09137f5a36b56dd5f2dff1790e
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 16:07:02 +0530

    Update provider.tf

--- 70-frontend-alb/provider.tf
+++ 70-frontend-alb/provider.tf
@@ -2,16 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.98.0"
+      
     }
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "s3bucketpolicytest-6792"
     key    = "roboshop-dev-ingress-alb"
     region = "us-east-1"
-    encrypt        = true
-    use_lockfile = true
+   
   }
 }
 
