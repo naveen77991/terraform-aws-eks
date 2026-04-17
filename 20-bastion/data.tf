29728f3702b939a6ee1e469bc9f729702ed5d243
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 15:56:43 +0530

    Update data.tf

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -8,3 +8,10 @@ data "aws_ami" "joindevops" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/roboshop/dev/bastion_sg_id"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/roboshop/dev/public_subnet_ids"
+}
