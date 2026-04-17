2cd357f23cd708cae0fd9b9ca4c4b719557fc9ed
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 17:25:59 +0530

    Create variables.tf

---
+++ 90-vpc-peering/variables.tf
@@ -0,0 +1,8 @@
+variable "requester_vpc_id" {}
+variable "accepter_vpc_id" {}
+
+variable "requester_route_table_id" {}
+variable "accepter_route_table_id" {}
+
+variable "requester_cidr" {}
+variable "accepter_cidr" {}
